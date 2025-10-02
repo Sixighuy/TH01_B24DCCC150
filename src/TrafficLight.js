d3e52505700de25a4e72665f21881753e2dbb615
Sixighuy <huyonlwork279@gmail.com>
2025-10-02 13:37:44 +0700

    Cập nhật code traffic light

--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -1,17 +1,24 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
 
 function TrafficLight() {
   const [light, setLight] = useState("red");
 
+  // Hàm đổi đèn
   const changeLight = () => {
     if (light === "red") setLight("green");
     else if (light === "green") setLight("yellow");
     else setLight("red");
   };
 
+  // Tự động chạy sau 2 giây
+  useEffect(() => {
+    const interval = setInterval(changeLight, 2000);
+    return () => clearInterval(interval); // clear khi component unmount
+  }, [light]);
+
   return (
-    <div style={{padding: 20}}>
-      <div style={{display: "flex", flexDirection: "column", width: 60}}>
+    <div style={{ padding: 20 }}>
+      <div style={{ display: "flex", flexDirection: "column", width: 60 }}>
         <div
           style={{
             width: 50,
@@ -40,7 +47,7 @@ function TrafficLight() {
           }}
         />
       </div>
-      <button onClick={changeLight} style={{marginTop: 10}}>
+      <button onClick={changeLight} style={{ marginTop: 10 }}>
         Chuyển đèn
       </button>
     </div>
